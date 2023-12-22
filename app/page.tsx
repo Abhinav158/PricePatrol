78c539fdcec9d7b3e3b69626c6ea8595902a6858
Abhinav Raghunandan <raghunandan.abhinav@gmail.com>
2023-12-22 14:07:23 +0530

    Added product details page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,14 @@ import HeroCarousel from "@/components/HeroCarousel"
 import Searchbar from "@/components/Searchbar"
 import Image from "next/image"
 
-const Home = () => {
+import { getAllProducts } from "@/lib/actions"
+import { get } from "http"
+import ProductCard from "@/components/ProductCard"
+
+const Home = async () => {
+  
+  const allProducts = await getAllProducts();
+
   return (
     <>
       <section className="px-6 md:px-20 py-24">
@@ -34,8 +41,8 @@ const Home = () => {
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
     
-          {['iPhone', 'Books', 'Shoes'].map((product) => (
-            <div>{product}</div>
+          {allProducts?.map((product: any) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
 
